6689c715ceec3b822e7e8814d919fbc515019e6c
Roque Laecio dos Santos <rlaecio@hotmail.com>
2022-08-24 22:45:18 +0100

    finish aula04

--- exercicio_17.py
+++ exercicio_17.py
@@ -1,16 +1,13 @@
 import os
 
-
 def limpar():
     os.system('cls' if os.name == 'nt' else 'clear') or None
 
-
-fatoral = 1
+fatorial = 1
 limpar()
 
 fator = int(input('\n Informe um numero para ser fatorado: '))
-
 for i in range(1, fator + 1):
-    fatoral *= i
-    
-print(fatoral)
+    fatorial *= i
+
+print(' O fatorial de',fator, 'é', fatorial, '\n')
